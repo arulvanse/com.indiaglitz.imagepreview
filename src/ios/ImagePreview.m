b806bd70ad964fe8400ec5ee8be37a81940d17a5
arulvanse <arul@vansesystems.com>
2015-07-23 18:09:23 +0530

    plugin commited
    plugin commited

--- src/ios/ImagePreview.m
+++ src/ios/ImagePreview.m
@@ -19,17 +19,28 @@
 - (void)openGallary:(CDVInvokedUrlCommand*)command{
     
     NSString* callbackId = [command callbackId];
-    NSArray* arrImages = [command arguments];
+   // NSArray* arrImages =[ [command arguments];
    // NSString* msg = [NSString stringWithFormat: @"Hello, %@", name];
    /* CDVPluginResult* result = [CDVPluginResult
                               resultWithStatus:CDVCommandStatus_OK
                               messageAsString:msg];
-    
     [self success:result callbackId:callbackId]; */
+    NSMutableArray *arrImages = [[NSMutableArray alloc]initWithObjects:
+                      [UIImage imageNamed:@"1.jpg"],
+                      [UIImage imageNamed:@"2.jpg"],
+                      [UIImage imageNamed:@"3.jpg"],
+                      [UIImage imageNamed:@"4.jpg"],
+                      [UIImage imageNamed:@"5.jpg"],
+                      [UIImage imageNamed:@"6.jpg"],
+                      [UIImage imageNamed:@"7.jpg"],
+                      [UIImage imageNamed:@"8.jpg"],
+                      [UIImage imageNamed:@"9.jpg"],
+                      nil];
+
     
     if ([arrImages count]>0) {
         GallaryVC *preview =[[ GallaryVC alloc] initWithNibName:@"GallaryVC" bundle:nil];
-        preview.arrImages=arrImages;
+        preview.arrImages=(NSArray *)arrImages;
         UIViewController *rootVC = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
         [rootVC presentViewController:preview animated:YES completion:nil];
         
