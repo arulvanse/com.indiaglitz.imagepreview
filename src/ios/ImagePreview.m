0b8e3e55abcca1e0ce8d211c81df54af5546b142
arulvanse <arul@vansesystems.com>
2015-07-24 08:11:56 +0530

    change 1

--- src/ios/ImagePreview.m
+++ src/ios/ImagePreview.m
@@ -28,7 +28,7 @@
    /* CDVPluginResult* result = [CDVPluginResult
                               resultWithStatus:CDVCommandStatus_OK
                               messageAsString:msg];
-    [self success:result callbackId:callbackId]; */
+    [self success:result callbackId:callbackId]; 
     NSMutableArray *arrImages = [[NSMutableArray alloc]initWithObjects:
                       [UIImage imageNamed:@"1.jpg"],
                       [UIImage imageNamed:@"2.jpg"],
@@ -40,20 +40,7 @@
                       [UIImage imageNamed:@"8.jpg"],
                       [UIImage imageNamed:@"9.jpg"],
                       nil];
-
-    
-    NSMutableArray *arrImages = [[NSMutableArray alloc]initWithObjects:
-                                 [UIImage imageNamed:@"1.jpg"],
-                                 [UIImage imageNamed:@"2.jpg"],
-                                 [UIImage imageNamed:@"3.jpg"],
-                                 [UIImage imageNamed:@"4.jpg"],
-                                 [UIImage imageNamed:@"5.jpg"],
-                                 [UIImage imageNamed:@"6.jpg"],
-                                 [UIImage imageNamed:@"7.jpg"],
-                                 [UIImage imageNamed:@"8.jpg"],
-                                 [UIImage imageNamed:@"9.jpg"],
-                                 nil];
-
+		*/
     
     if ([arrImages count]>0) {
         GallaryVC *preview =[[ GallaryVC alloc] initWithNibName:@"GallaryVC" bundle:nil];
